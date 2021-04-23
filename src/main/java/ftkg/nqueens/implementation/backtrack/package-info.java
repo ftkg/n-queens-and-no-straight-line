24520f0de0f53e92b4b5a1f5d49a46332c3a1cb1
ftkg <fer.nando36@gmail.com>
2021-04-23 13:00:42 -0300

    additional package-info comment

--- src/main/java/ftkg/nqueens/implementation/backtrack/package-info.java
+++ src/main/java/ftkg/nqueens/implementation/backtrack/package-info.java
@@ -1,5 +1,7 @@
 /**
  * This module contains implementations for the N-Queen* problem using backtracking, and additional model classes.
  * <p>* with the additional constraint of no 3 queens forming straight lines
+ * <p> The backtracking starts on the leftmost column and tries to place a queen iteratively in each column to the
+ * right. If it fails, it backtracks, trying every configuration until it finds a solution (or no solution).
  */
 package ftkg.nqueens.implementation.backtrack;
